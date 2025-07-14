9c149513a203f42ff4b91ba35b9ff707fce948b2
AjinkyaSanjayPatil <ajinkyaas8900@gmail.com>
2025-07-14 17:25:45 +0530

    media
    priaccteam2

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Chatbot from 'C:/Users/HP/Desktop/Priacc_team2/PriaccEnterpriseAI/frontend/src/components/chat/Chatbot.js';
+ 
+function App() {
+  return (
+<div className="App">
+<Chatbot />
+</div>
+  );
+}
+ 
+export default App;
